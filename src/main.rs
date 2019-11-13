44e8d59af49fe9f0051429391f3af0d989661056
zc <zhichuan.liang@shopee.com>
2019-11-13 16:55:29 +0800

    parse cli args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli.yaml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    println!(
+        "config: {}",
+        matches.value_of("config").unwrap_or("/etc/eru/agent.yaml")
+    );
+    println!(
+        "health_check_interval: {}",
+        matches.value_of("health_check_interval").unwrap()
+    );
+    if let Some("yes") = matches.value_of("log_stdout") {
+        println!("log stdout: yes!");
+    } else {
+        println!("log stdout: false");
+    }
 }
